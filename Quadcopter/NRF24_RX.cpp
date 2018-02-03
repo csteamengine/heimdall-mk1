bfac9b666dbd11445419ac5fbc41e98ede9f2025
Gregory Steenhagen <gregorysteenhagen@Gregorys-MacBook-Pro.local>
2018-02-02 23:29:20 -0600

    Added code for reading the rest of the buttons. wrote struct, but haven't tested it

--- Quadcopter/NRF24_RX.cpp
+++ Quadcopter/NRF24_RX.cpp
@@ -17,6 +17,24 @@ const byte address[6] = "00001";
 RF24 radio(7, 8); // CE, CSN
 uint16_t values[8];
 
+struct packet {
+  uint8_t throttle;
+  uint8_t yaw;
+  uint8_t pitch;
+  uint8_t roll;
+  boolean plus = false;
+  boolean minus = false;
+  boolean homebutton = false;
+  boolean dup = false;
+  boolean dleft = false;
+  boolean ddown = false;
+  boolean dright = false;
+  boolean xbutton = false;
+  boolean circle = false;
+  boolean triangle = false;
+  boolean square = false;
+};
+
 packet nrf24Data;
 
 void resetRF24Data() 
@@ -41,9 +59,11 @@ void NRF24_Read_RC() {
 
   unsigned long now = millis();
   if ( radio.available() ) {
+//    TODO replace this with the packet struct
     radio.read(&values, sizeof(values));
 
     lastRecvTime = now;
+//    TODO replace this with the packet struct
     nrf24_rcData[THROTTLE] = map(values[3], 0, 255, 1000, 2000);
     nrf24_rcData[YAW] =      map(values[2],      0, 1024, 1000, 2000);
     nrf24_rcData[PITCH] =    map(values[1],    0, 255, 1000, 2000);
